e7373725e29556446b18fff6f1d6d7211cd20d22
Wolmir Nemitz <wolmir.nemitz@gmail.com>
2016-11-24 22:38:01 -0200

    Add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var path = require('path');
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ArchivePlugin = require('webpack-archive-plugin');
+
+var config = require('./config');
+
+if (config.ambiente === 'PRODUCAO') {
+	module.exports = {
+		entry: './app/index.js',
+		output: {
+			path: __dirname + '/dist',
+			filename: 'app.bundle.js'
+		},
+
+		resolve: {
+			root: path.resolve(__dirname),
+			alias: {
+				lib: 'lib/js',
+				config: 'config'
+			}
+		},
+
+
+		plugins: [
+			new CopyWebpackPlugin([
+				{from: __dirname + '/lib', to:'lib'}
+			]),
+
+			new ArchivePlugin()
+		]
+	};
+}
+
+else if (config.ambiente === 'DESENVOLVIMENTO') {
+	module.exports = {
+		entry: './app/index.js',
+		output: {
+			path: __dirname + '/dist',
+			filename: 'app.bundle.js'
+		},
+
+		resolve: {
+			root: path.resolve(__dirname),
+			alias: {
+				lib: 'lib/js',
+				config: 'config'
+			}
+		},
+
+
+		plugins: [
+			new CopyWebpackPlugin([
+				{from: __dirname + '/lib', to:'lib'}
+			])
+		]
+	};
+}
