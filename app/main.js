48087ed707e178a0c0397a06151269dade0e7f9c
Wolmir Nemitz <w.neto@cinq.com.br>
2016-12-01 14:21:48 -0200

    Add componente de tabela

--- app/main.js
+++ app/main.js
@@ -4,56 +4,7 @@ import {run} from '@cycle/xstream-run';
 import {div, section, a, span, i, nav, header, h5, h3, label, input, table, th, td, tr, thead, tbody, button, footer, makeDOMDriver} from '@cycle/dom';
 import TopNav from './top-nav.component';
 import SideNav from './side-nav.component';
-
-
-var viewNav = function() {
-	return nav('.w3-sidenav.w3-theme-d1.w3-card-8.w3-large', {style: {width: '20%'}}, [
-				header('.w3-container.w3-theme-d4', [
-					h5('Menu SEG')
-				]),
-				div({style: {'margin-top': '2em'}}, [
-					a({style: {'cursor': 'pointer'}}, [span('.fa.fa-file-word-o', {style: {color:'#000000', 'margin-bottom': '1em'}}), ' Atestados']),
-					a({style: {'cursor': 'pointer'}}, [span('.fa.fa-file-text-o', {style: {color:'#000000', 'margin-bottom': '1em'}}), ' Relatórios'])
-				])
-			]);
-};
-
-
-var viewInputPesquisa = function() {
-	return div('.w3-container.w3-section', {style: {position: 'relative'}}, [
-				label('.w3-label', [
-					span('.fa.fa-search'),
-					' Pesquisar'
-				]),
-				input('.w3-input', {props: {type: 'text'}})
-			]);
-};
-
-var viewTable = function() {
-	return table('.w3-table.w3-bordered.w3-striped.w3-hoverable', [
-				thead([
-					tr('.w3-theme-d1', [
-						th('Aluno'),
-						th('Matrícula'),
-						th('Projeto'),
-						th('Coordenador'),
-						th('Ações')
-					])
-				]),
-				tbody([
-					tr([
-						td('Juliano'),
-						td('1115143068'),
-						td('Programa'),
-						td('Aline'),
-						td([
-							button('.w3-btn.w3-red', [i('.fa.fa-trash')]),
-							button('.w3-btn.w3-dark-grey.w3-hover-cyan', [i('.fa.fa-edit')])
-						]),
-					])
-				])
-			]);
-};
+import DataGrid from './data-grid.component';
 
 function main(sources) {
 	const topNavProp$ = xs.of({
@@ -89,29 +40,44 @@ function main(sources) {
 	const sideNav = SideNav({DOM: sources.DOM, props: sideNavProp$, options: sideNavOption$});
 	const sideNavDom$ = sideNav.DOM;
 
+	const dataGridProp$    = xs.of({title: 'Lista de Atestados'});
+	const dataGridColumns$ = xs.of([
+		{ label: 'Nome', name: 'nome' }, 
+		{ label: 'Matrícula', name: 'matricula' },
+		{ label: 'Projeto', name: 'projeto' }, 
+		{ label: 'Coordenador(a)', name: 'coordenador' }
+	]);
+	const dataGridData$ = xs.fromArray([
+		{
+			nome: 'Stephen Strange',
+			matricula: '101678743',
+			projeto: 'Gambis da Magia',
+			coordenador: 'Aline Mello'
+		}
+	]);
+	const dataGridActions$ = xs.of([
+		{icon: 'fa-trash', color: 'w3-red'},
+		{icon: 'fa-edit', color: 'w3-dark-grey'}
+	]);
+
+	const dataGrid = DataGrid({
+		DOM: sources.DOM,
+		columns: dataGridColumns$, 
+		data: dataGridData$,
+		props: dataGridProp$,
+		actions: dataGridActions$
+	});
+
+	const dataGridDom$ = dataGrid.DOM;
+
 	return {
-		DOM: xs.combine(topNavDom$, sideNavDom$).map(([topNavDom, sideNavDom]) =>
+		DOM: xs.combine(topNavDom$, sideNavDom$, dataGridDom$).map(([topNavDom, sideNavDom, dataGridDom]) =>
 			div('#bn-sistema-wrapper', [
 				section('.w3-row', [
 					topNavDom,
-					// viewMenuTopo(),
 					div('#bn-sistema-conteudo', [
-						// viewNav(),
 						sideNavDom,
-						section('.w3-container.w3-row', {style:{'margin-left': '20%'}}, [
-							section('.w3-section.w3-card-16.w3-col.l12.m12.s12', [
-								header('.w3-container.w3-theme-d4.w3-padding', [
-									h3('Lista de Atestados')
-								]),
-								section('.w3-row', [
-									section('.w3-col.l12.m12.s12', [
-										viewInputPesquisa(),
-										viewTable()
-									])
-								]),
-								footer('.w3-theme-d4.w3-center', [i('.fa.fa-pagelines')])
-							])
-						])
+						dataGridDom
 					])
 				])
 			])
